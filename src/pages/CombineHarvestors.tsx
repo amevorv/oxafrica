3e87b48e4efa4041b467fd984a6c7c5693a53b29
Victor <69175873+BrumNet@users.noreply.github.com>
2025-11-09 20:11:27 +0000

    refactor(CombineHarvestors): replace multiple components with CombineHarvestorContent
    Simplify the CombineHarvestors page by consolidating multiple components into a single CombineHarvestorContent component for better maintainability

--- src/pages/CombineHarvestors.tsx
+++ src/pages/CombineHarvestors.tsx
@@ -1,17 +1,16 @@
-import { BasicContentWithImages } from "../components/BasicContentWithImages"
-import { BreadCrumb } from "../components/BreadCrumb"
+// import { BasicContentWithImages } from "../components/BasicContentWithImages"
+// import { BreadCrumb } from "../components/BreadCrumb"
 import { Footer } from "../components/Footer"
 import { Header } from "../components/Header"
-import { HeroImage2 } from "../components/HeroImage2"
-import image16 from '../images/home/image16.png'
+import { CombineHarvestorContent } from "../components/CombineHarvestorContent"
+// import { HeroImage2 } from "../components/HeroImage2"
+// import image16 from '../images/home/image16.png'
 
 export const CombineHarvestors = () => {
     return (
             <>
                 <Header/>
-                <HeroImage2 url={image16}/>
-                <BreadCrumb target="Combine Harvestors"/>
-                <BasicContentWithImages equipment="combine"/>
+                <CombineHarvestorContent />
                 <Footer/>
             </>
         )
