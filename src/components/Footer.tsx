6ea5c189a4c0bfdecf2ca170bec6f9c90f2b87bf
Victor <69175873+BrumNet@users.noreply.github.com>
2025-11-09 07:41:52 +0000

    feat(Footer): redesign footer with bootstrap components and social icons
    - Replace plain HTML with responsive Bootstrap layout
    - Add social media icons and newsletter subscription form
    - Improve visual hierarchy and styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,57 @@
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
+
 export const Footer = () => {
-    return (
-        <div id="footer">
-            <div id="footerup"> <div> <a>Products & Support</a><br/> <a>Equipment</a><br/> <a>Buy Parts</a><br/> <a>Find a Dealer</a><br/> <a>Buy Online</a><br/> <a>Build Your Own</a><br/> <a>Company Information</a><br/> <a>About Our Company</a><br/> <a>Our Higher Purpose</a><br/> <a>Innovation</a> </div> <div> <a>Electrification</a><br/> <a>Sustainability</a><br/> <a>Diversity Equity & Inclusion</a><br/> <a>Careers</a><br/> <a>Governance</a><br/> <a>Investor Relations</a><br/> <a>News & Publications</a><br/> <a>The Furrow</a> </div> <div> <a>The Dirt</a><br/> <a>The Landing</a><br/> <a>Power Connect</a><br/> <a>Factory Tours</a><br/> <a>Buy Merchandise</a> </div> </div>
-            <div id="footerdown">
-                {/* <p><a>Site Map</a> <a>Privacy</a> <a>Cookie Statement</a> <a>Terms of Use</a> <a>Accessibility</a> <a>Contact Us</a></p> */}
-                <p>Cookie Preferences</p>
-                <p>OX Engineering. Copyright @ 2025. All rights reserved</p>
+  return (
+    <footer className="bg-success text-white mt-5 py-5">
+      <Container>
+        <Row>
+          <Col md={3}>
+            <h5>Products & Support</h5>
+            <ul className="list-unstyled">
+              <li className="mb-2"><a href="#" className="text-white-50 text-decoration-none hover:text-white">Equipment</a></li>
+              <li className="mb-2"><a href="#" className="text-white-50 text-decoration-none hover:text-white">Buy Parts</a></li>
+              <li className="mb-2"><a href="#" className="text-white-50 text-decoration-none hover:text-white">Find a Dealer</a></li>
+              <li className="mb-2"><a href="#" className="text-white-50 text-decoration-none hover:text-white">Buy Online</a></li>
+            </ul>
+          </Col>
+          <Col md={3}>
+            <h5>Company Information</h5>
+            <ul className="list-unstyled">
+              <li className="mb-2"><a href="#" className="text-white-50 text-decoration-none hover:text-white">About Our Company</a></li>
+              <li className="mb-2"><a href="#" className="text-white-50 text-decoration-none hover:text-white">Careers</a></li>
+              <li className="mb-2"><a href="#" className="text-white-50 text-decoration-none hover:text-white">Investor Relations</a></li>
+              <li className="mb-2"><a href="#" className="text-white-50 text-decoration-none hover:text-white">News & Publications</a></li>
+            </ul>
+          </Col>
+          <Col md={3}>
+            <h5>Connect with Us</h5>
+            <div className="d-flex gap-3">
+              <a href="#" className="text-white-50 text-decoration-none hover:text-white"><FaFacebook size={24} /></a>
+              <a href="#" className="text-white-50 text-decoration-none hover:text-white"><FaTwitter size={24} /></a>
+              <a href="#" className="text-white-50 text-decoration-none hover:text-white"><FaInstagram size={24} /></a>
+              <a href="#" className="text-white-50 text-decoration-none hover:text-white"><FaLinkedin size={24} /></a>
             </div>
-        </div>
-        
-    )
-}
+          </Col>
+          <Col md={3}>
+            <h5>Newsletter</h5>
+            <p className="text-white-50 mb-3">Stay up to date with our latest news and offers.</p>
+            <Form>
+              <Form.Group controlId="formBasicEmail" className="mb-3">
+                <Form.Control type="email" placeholder="Enter email" className="bg-light text-dark border-0" />
+              </Form.Group>
+              <Button variant="light" type="submit" className="w-100 text-primary fw-bold">
+                Subscribe
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+        <Row className="mt-4">
+          <Col className="text-center">
+            <p>&copy; 2025 OX Engineering. All rights reserved.</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
