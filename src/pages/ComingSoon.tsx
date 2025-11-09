ff344c2025171f4aec612672779b5c3aad3f9d45
Victor <69175873+BrumNet@users.noreply.github.com>
2025-11-09 07:42:12 +0000

    refactor(ComingSoon): simplify layout and remove HeroImage component
    Remove unnecessary HeroImage component and streamline the layout styling by replacing flex properties with simpler text alignment and padding

--- src/pages/ComingSoon.tsx
+++ src/pages/ComingSoon.tsx
@@ -1,19 +1,14 @@
 import { ComingSoonContent } from "../components/comingsoon"
 import { Footer } from "../components/Footer"
 import { Header } from "../components/Header"
-import { HeroImage } from "../components/HeroImage"
 
 export const ComingSoon = () => {
     return (
         <>
             <Header />
-            <HeroImage/>
-            <div style={{display: "flex",
-            justifyContent: "center",
-            alignItems: "center", width: "100%"}}>
+            <div style={{textAlign: "center", padding: "50px"}}>
                 <ComingSoonContent/>
             </div>
-            
             <Footer />
         </>
     )
